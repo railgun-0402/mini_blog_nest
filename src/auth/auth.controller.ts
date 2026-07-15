7029b55dd1ae98d539708628791cfa147f77c4e2
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2026-07-15 23:47:44 +0900

    lint err repair

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, HttpCode, Post, Res, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Post,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
 import type { Response } from 'express';
 import { AuthService } from './auth.service';
 import { CurrentUserDecorator } from './decorators/current-user.decorator';
